80ef2186f0a464e87552b02e1bac424916e05dbd
Gonzalo <gonzalobarravalle@gmail.com>
2023-08-17 20:25:14 -0400

    agregado cors

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const cors = require('cors')
+
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -16,6 +18,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(express.json());
 app.use(requestLogger)
+app.use(cors())
 
 let notes = [
   {
